d2618a981d6bd6e494d4cec984ed3238438f167f
ashabayev <annashabayev@gmail.com>
2016-03-15 23:42:40 -0500

    pokemonCenter and TestClass

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,51 +1,9 @@
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 
 public class TestClass {
-	public static void main(String[] args) throws IOException	
-	{
-		String fileName = "pokemonData.dat";
-		try
-		{
-
-			byte[] buffer = new byte[1000];
-			FileInputStream fStream = new FileInputStream(fileName);
-			DataInputStream inFile = new DataInputStream(fStream);
-
-			while(true){
-				try
-				{inFile.readInt();}
-				catch(Exception e)
-				{
-					break;
-				}
-			}
-
-
-			int total = 0;
-			int nRead = 0;
-			while((nRead = fStream.read(buffer)) != -1) {
-				System.out.println(new String(buffer));
-				total += nRead;
-			}
-			fStream.close();
-
-			System.out.println("Read " + total + " bytes");
-
-
-		}
-		catch  (FileNotFoundException e)
-		{
-			System.out.println("Unable to open file '" + fileName + "'");
-		}
-		catch(IOException e) {
-			System.out.println( "Error reading file '" + fileName + "'");
-		}
-
-
+	
+	
+public void intro(){
 		System.out.println("#############################################################################################################");
 		System.out.println("");
 		System.out.println("Welcome to the PokéCenter! ");
@@ -55,5 +13,5 @@ public class TestClass {
 		System.out.println("You will also now return here if all your Pokémon faint at once! ");
 		System.out.println("We hope to see you again soon. ");
 		System.out.println("");
-	}
+}
 }
---
+++ src/pokemonCenter.java
@@ -0,0 +1,50 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class pokemonCenter{
+	public static void main(String[] args) throws IOException	
+{
+	String fileName = "pokemonData.dat";
+	try
+	{
+		byte[] buffer = new byte[1000];
+		FileInputStream fStream = new FileInputStream(fileName);
+		DataInputStream inFile = new DataInputStream(fStream);
+
+		while(true){
+			try
+			{inFile.readInt();}
+			catch(Exception e)
+			{
+				break;
+			}
+		}
+		int total = 0;
+		int nRead = 0;
+		while((nRead = fStream.read(buffer)) != -1) {
+			System.out.println(new String(buffer));
+			total += nRead;
+		}
+		fStream.close();
+
+		System.out.println("Read " + total + " bytes");
+
+
+	}
+	catch  (FileNotFoundException e)
+	{
+		System.out.println("Unable to open file '" + fileName + "'");
+	}
+	catch(IOException e) {
+		System.out.println( "Error reading file '" + fileName + "'");
+	}
+
+	/**PokemonCenter(String inputFile); 
+	void printPokemonInfoByName(String name); 
+	void printPokemonInfoByOwner(String name); 
+	void printPokemonBoarding(int   month, int day, year); 
+*/
+}
+}
