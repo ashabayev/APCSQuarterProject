89f63c6f10a73f956c386c4a37f866e6aec0f749
noexitorion <undyinggnostic@gmail.com>
2016-03-15 23:17:28 -0500

    Update and rename Mareep to Mareep.java

---
+++ src/Mareep.java
@@ -0,0 +1,18 @@
+public class Mareep extends Pokemon
+{
+private boolean isClipped;
+public Mareep()
+{
+}
+
+public void setClipped(boolean yes)
+{
+isClipped = yes;
+}
+
+public boolean getClipped()
+{
+return isClipped;
+}
+
+}
