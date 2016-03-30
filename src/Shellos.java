e2822784193ce5fb07b95088e2050df76d031ac2
noexitorion <undyinggnostic@gmail.com>
2016-03-29 23:52:26 -0500

    Create Shellos.java

---
+++ src/Shellos.java
@@ -0,0 +1,18 @@
+public class Shellos extends Pokemon
+{
+private int eastWest;
+public Shellos()
+{
+
+}
+
+public void seteastWest(int i)
+{
+eastWest = i;
+}
+
+public void geteastWest(int i)
+{
+}
+
+}
